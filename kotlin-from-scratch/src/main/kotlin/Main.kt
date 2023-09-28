3675927432459eaddca12841d32bfe49d7e239fb
Krishna Lagad <krishnalagad2@gmail.com>
2023-09-28 18:26:37 +0530

    - dump main kotlin file

---
+++ kotlin-from-scratch/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("Hello Kotlin")
+
+    val name = "Krishna Lagad"
+    val email = "krishna@gmail.com"
+    var age = 22
+    println("My name is $name. My email is $email and my age is $age")
+
+    age = 23
+    println("My name is $name. My email is $email and my age is $age")
+}
