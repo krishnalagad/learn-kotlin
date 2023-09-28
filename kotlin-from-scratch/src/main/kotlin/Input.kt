96169b24372b9d60a8d3ea63a2d6641acf50c3f3
Krishna Lagad <krishnalagad2@gmail.com>
2023-09-28 19:40:28 +0530

    - taking command line input from user

---
+++ kotlin-from-scratch/src/main/kotlin/Input.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Enter your name:")
+    val name: String = readln()
+    println("Enter your age:")
+    val age: Int = readln().toInt()
+    println("Enter mobile number:")
+    val mobile: String = readln()
+
+    println("$name -> $age -> $mobile")
+}
