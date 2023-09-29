e1d962ddbd6daee1fbf3f6361db5e87e8d61eb16
Krishna Lagad <krishnalagad2@gmail.com>
2023-09-29 13:38:16 +0530

    - dump variable arguments

--- kotlin-from-scratch/src/main/kotlin/Lambda.kt
+++ kotlin-from-scratch/src/main/kotlin/Lambda.kt
@@ -2,9 +2,18 @@ fun main() {
     cal(89, 43) {
         println("Addition completed")
     }
+    add(12, 23, 30, 45, 98)
 }
 
 fun cal(a: Int, b: Int, callback: () -> Unit) {
     println("Addition is ${a + b}")
     callback()
+}
+
+fun add(vararg nums: Int) {
+    var sum = 0
+    for (value in nums) {
+        sum += value
+    }
+    println(sum)
 }
